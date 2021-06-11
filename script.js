80c3d1b542972ec734316afdd32f9ca2c58c5d47
Joseph Manning <jgmanning3@yahoo.com>
2021-06-11 16:41:47 -0700

    Capture data from search form

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var APIKey = "f57bb5add0d36df3df9a18537e407ac9";
-var city = document.querySelector("#citySearch");
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=f57bb5add0d36df3df9a18537e407ac9";
-var searchBtn = document.querySelector("#searchBtn");
+debugger
+var city = "";
+function writeCity() {
+    city = document.querySelector("#citySearch");
+    console.log(city.value);
+    alert(city.value);
+};
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f57bb5add0d36df3df9a18537e407ac9";
+// var searchBtn = document.querySelector("#searchBtn");
+
+// var queryLatLong = "http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=f57bb5add0d36df3df9a18537e407ac9";
+// console.log(queryLatLong)
 // var citySearch = document.querySelector("#citySearch");
 
-searchBtn.addEventListener("click", function(){
-    fetch(queryURL)
-    .then(response => response.json())
-    .then(data => console.log(data))
+// function getWeatherData(){
+//     fetch(queryLatLong)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// }
 
-    .catch(err => alert("Wrong city name!"))
+// buttonContainer.addEventListener("click", function(e){
+//     getWeatherData(cityName)
+// })
 
-})
+// FormData.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     getWeatherData(cityName);
+// })
