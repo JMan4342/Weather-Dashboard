aaeb0120f52e3397fdd54c1d5dce15a178137e4c
Joseph Manning <jgmanning3@yahoo.com>
2021-06-15 14:46:32 -0700

    Pull city name from API data

--- script.js
+++ script.js
@@ -27,101 +27,99 @@ function fetchLatLong(event) {
 // Capture current and forcast weather
 function fetchWeather(data) {
   var { lat, lon } = data[0];
+  var { name } = data[0];
+  document.querySelector("#currentCity").innerText = name;
 
   console.log(lat, lon);
 
   fetch(
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       lat +
       "&lon=" +
       lon +
       "&exclude=minutely,hourly,alerts&units=imperial&appid=" +
       apiKey
-  )
-    .then((response) => {
-      return response.json();
-    })
-    // .then((data) => console.log(data))
-    // Send to current weather function
-    .then((data) => {
-      displayForcast(data);
-      displayCurrent(data);
-    });
-
-  // Send to forcast function
-}
-
-// fetchWeather()
-
-function displayCurrent(data) {
-  var { dt, temp, humidity, wind_speed, uvi } = data.current;
-  var { icon } = data.current.weather[0];
-  var city = document.querySelector("#citySearch").value;
-  var date = new Date(1623686400 * 1000);
-  console.log(date);
-  var formatDate =
-    date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
-  console.log(formatDate);
-
-  console.log(temp, humidity, wind_speed, uvi, icon, city);
-
-  // May need to rename city variable
-  document.querySelector("#currentCity").innerText = city;
-
+      )
+      .then((response) => {
+          return response.json();
+        })
+        // .then((data) => console.log(data))
+        // Send to current weather function
+        .then((data) => {
+            displayForcast(data);
+            displayCurrent(data);
+        });
+        
+        // Send to forcast function
+    }
+    
+    // fetchWeather()
+    
+    function displayCurrent(data) {
+        var { dt, temp, humidity, wind_speed, uvi } = data.current;
+        var { icon } = data.current.weather[0];
+        var city = document.querySelector("#citySearch").value;
+        var date = new Date(dt * 1000);
+        console.log(date);
+        var formatDate =
+        date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+        console.log(formatDate);
+        
+        console.log(temp, humidity, wind_speed, uvi, icon, city);
+        
   document.querySelector("#currentDate").innerText = formatDate;
   document.querySelector("#currentIcon").src =
     "http://openweathermap.org/img/wn/" + icon + ".png";
   document.querySelector("#currentTemp").innerHTML = temp + "&#8457;";
-  document.querySelector("#currentHumid").innerText = humidity + "%";
-  document.querySelector("#currentWindSpeed").innerText = wind_speed + " MPH";
-  document.querySelector("#currentUVI").innerText = uvi;
+  document.querySelector("#currentHumid").innerText =
+    "Humidity: " + humidity + "%";
+  document.querySelector("#currentWindSpeed").innerText =
+    "Wind Speed: " + wind_speed + " MPH";
+  document.querySelector("#currentUVI").innerText = "UV Inded: " + uvi;
 }
 
+// Display 5-day forcast
 function displayForcast(data) {
-    var container = document.querySelector("#multiDayForcast");
-    var forcastData = [
-      "#forcastDate0",
-      "#forcastTempHigh0",
-      "#forcastTempLow0",
-      "#forcastIcon0",
-      "#forcastHumid0",
-      "#forcastWindSpeed0",
-    ];
-    
-    // Forcast data will be looped through here, use similar coding as display current
-    for (var i = 0; i < 5; i++) {
-    var nameEl = document.createElement("p");
-    nameEl.innerText = forcastData;
-    container.appendChild(nameEl);
+  // Forcast data will be looped through here, use similar coding as display current
+  var multiDayForcast = document.querySelector("#multiDayForcast");
 
-    var { dt, humidity, wind_speed } = data.daily[0];
-    var { icon } = data.daily[0].weather[0];
-    var { max, min } = data.daily[0].temp;
-    var city = document.querySelector("#citySearch").value;
-    var date = new Date(dt * 1000);
-    console.log(date);
-    var formatDate =
-      date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
-    console.log(formatDate);
+//   Pull five days worth of data from API
+  for (var i = 0; i < 5; i++) {
+  var { dt, humidity, wind_speed } = data.daily[i];
+  var { icon } = data.daily[i].weather[0];
+  var { max, min } = data.daily[i].temp;
+  var date = new Date(dt * 1000);
+  console.log(date);
+  var formatDate =
+    date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+  console.log(formatDate);
+  var city = document.querySelector("#citySearch").value;
+  
+// Display data on page
 
-    console.log(max, min, humidity, wind_speed, icon, city);
-
-    document.querySelector("#forcastDate0").innerText = formatDate;
-    document.querySelector("#forcastIcon0").src =
-      "http://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector("#forcastTempHigh0").innerHTML = max + "&#8457;";
-    document.querySelector("#forcastTempLow0").innerHTML = min + "&#8457;";
-    document.querySelector("#forcastHumid0").innerText = humidity + "%";
-    document.querySelector("#forcastWindSpeed0").innerText =
-      wind_speed + " MPH";
+//   var multiEl = document.createElement("p");
+//     multiEl.innerText = multiDayForcast[i];
+//     multiDayForcast.appendChild(multiEl);
+  
+  console.log(max, min, humidity, wind_speed, icon, city);
   }
+
+//   Gets City name from search
+
+//   Converts unix date to readable date
+
+//   Display data on page on multiDayForcast id
 }
 
-// const obj = {
-//     a:function(){},
-//     b:function(){
-//         this.a()
-//     }
+// document.querySelector("#forcastDate0").innerText = formatDate;
+// document.querySelector("#forcastIcon0").src =
+// "http://openweathermap.org/img/wn/" + icon + ".png";
+// document.querySelector("#forcastTempHigh0").innerHTML = max + "&#8457;";
+// document.querySelector("#forcastTempLow0").innerHTML = min + "&#8457;";
+// document.querySelector("#forcastHumid0").innerText =
+// "Humidity: " + humidity + "%";
+// document.querySelector("#forcastWindSpeed0").innerText =
+// "Wind Speed: " + wind_speed + " MPH";
 // }
 
 document.querySelector("#searchBtn").addEventListener("click", fetchLatLong);
